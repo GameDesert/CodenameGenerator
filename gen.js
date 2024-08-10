d18a536c84aace947dd50b1d235db5065d308976
GameDesert <56128805+GameDesert@users.noreply.github.com>
2024-08-10 20:34:00 +0100

    Added rate limiter

--- gen.js
+++ gen.js
@@ -3,6 +3,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+import { rateLimit } from 'express-rate-limit'
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    message: "Too many requests, please try again later.",
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
 app.get('/', (req, res) => {
     const result = generate();
     res.send(result);
